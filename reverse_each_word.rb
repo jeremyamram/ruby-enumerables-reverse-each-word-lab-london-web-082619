46b922f512ea357574d41e915be99f8439d0f1d8
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-25 21:40:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,10 @@ reverse_each_word("hey there")
 
 
 
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
+end
+reverse_each_word("hey there")
