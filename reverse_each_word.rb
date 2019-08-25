3286ea2cc876677b1477bc0ee35eddcfe7c90c14
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-25 21:28:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,11 @@
 def reverse_each_word(sentenced)
-  new = sentenced.split(/ /)
-  new.each do |n|
-    n.reverse
+  new = sentenced.split(/ /).each do |word|
+  word.reverse
   end
-  new.join(" ")
+  word.join(" ")
 end
 reverse_each_word("hey there")
 
 
-def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
-  word.reverse
-end
-  reversed.join(" ")
-end
+
 
